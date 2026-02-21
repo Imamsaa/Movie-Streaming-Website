c36bf94d56112b595faab1304f4de81c5b08a509
Imamsaa <imamsafii2222022@gmail.com>
2026-02-21 10:39:54 +0700

    feat : Create Subscribetion plans

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscribeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/subscribe/plans', [SubscribeController::class, 'showplans'])->name('subscribe.plans');
