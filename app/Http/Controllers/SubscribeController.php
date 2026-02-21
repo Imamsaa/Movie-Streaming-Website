617d279870431c46577f16245238bdc08c5c7dfb
Imamsaa <imamsafii2222022@gmail.com>
2026-02-21 21:18:27 +0700

    feat : Implementasi redirect middleware auth

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Plan;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class SubscribeController extends Controller
+class SubscribeController extends Controller implements HasMiddleware
 {
+    public static function middleware()
+    {
+        return ['auth'];
+    }
+
     public function showplans()
     {
         $plans = Plan::all();
